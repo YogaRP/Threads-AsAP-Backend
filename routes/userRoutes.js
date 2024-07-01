0ac950faa969060370505a7bbb6eabb1290969ed
yogarp <yogarizky51@gmail.com>
2024-07-01 14:30:39 +0700

    func user,post,auth,middleware,jwt

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  followUnfollowUser,
+  getUserProfile,
+  loginUser,
+  logoutUser,
+  signUpUser,
+  updateUser,
+} from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+router.post("/signup", signUpUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnfollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id", protectRoute, updateUser);
+
+export default router;
